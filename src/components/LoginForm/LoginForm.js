398412fcb500bfe38b75eb5cf9455f7dfadd4441
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-14 08:12:50 -0700

    Added a login page with a login form. Both unstyled currently.

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+//import TokenService from '../../services/token-service';
+//import AuthApiService from '../../services/auth-api-service';
+import './LoginForm.css'
+
+export default class LoginForm extends Component {
+  static defaultProps = {
+    onLoginSuccess: () => {}
+  };
+
+  state = {error: null};
+
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault();
+    /* TODO Uncomment when login services are ready.
+    this.setState({error: null});
+    const {user_name, password} = ev.target;
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+      .then(res => {
+        user_name.value = '';
+        password.value = '';
+        TokenService.saveAuthToken(res.authToken);
+        this.props.onLoginSuccess();
+      })
+      .catch(res => {
+        this.setState({error: res.error});
+      });
+    */
+  };
+
+  render() {
+    const {error} = this.state;
+    return (
+      <>
+        <div role='alert'>
+          {error && <p className='red'>{error}</p>}
+        </div>
+        <form className='LoginForm'
+          onSubmit={this.handleSubmitJwtAuth}
+        >
+          <div className='user_name'>
+            <label htmlFor='LoginForm__user_name'>
+              User name:
+            </label>
+            <input
+              name='user_name'
+              required
+              id='LoginForm__user_name'
+            />
+          </div>
+          <div className='password'>
+            <label htmlFor='LoginForm__password'>
+              Password:
+            </label>
+            <input
+              name='password'
+              type='password'
+              required
+              id='LoginForm__password'
+            />
+          </div>
+          <button type='submit'>
+            Login
+          </button>
+        </form>
+      </>
+    );
+  };
+};
