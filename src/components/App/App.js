63de709a7b5e2d61c73d9aa882aec4ca0819dec8
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-14 12:50:58 -0700

    Final merge stuff

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom'
+import Header from '../Header/Header'
+// import PrivateRoute from '../PrivateRoute/PrivateRoute'
+import PublicOnlyRoute from '../PublicOnlyRoute/PublicOnlyRoute'
+import LandingPage from "../../routes/LandingPage/LandingPage";
+import RegistrationRoute from '../../routes/RegistrationRoute/RegistrationRoute'
+import NotFoundRoute from '../../routes/NotFoundRoute/NotFoundRoute'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+          <Switch>
+            <Route
+              exact path={'/'}
+              component={LandingPage}
+            />
+            <PublicOnlyRoute
+              path={'/register'}
+              component={RegistrationRoute}
+            />
+            <Route
+              component={NotFoundRoute}
+            />
+          </Switch>
+        </main>
     </div>
   );
 }
