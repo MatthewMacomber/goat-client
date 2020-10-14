5a3c508e1643c5d41ceea18c5f5113e7666216db
Justin Foster <jfoster91@gmail.com>
2020-10-14 12:06:02 -0400

    Added registration

---
+++ src/routes/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = (props) => {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/services/auth-api-service.js
@@ -0,0 +1,75 @@
+import config from '../config'
+import TokenService from './token-service'
+
+const AuthApiService = {
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/user`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  postLogin({ username, password }) {
+    return fetch(`${config.API_ENDPOINT}/auth/token`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(err => Promise.reject(err))
+          : res.json()
+      )
+  },
+  refreshToken() {
+    return fetch(`${config.API_ENDPOINT}/auth/token`, {
+      method: 'PUT',
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getWords() {
+    return fetch(`${config.API_ENDPOINT}/language`, {
+      method: 'GET',
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+  },
+  getHead() {
+    return fetch(`${config.API_ENDPOINT}/language/head`, {
+      method: 'GET',
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+  },
+  postGuess(guess, wordId) {
+    return fetch(`${config.API_ENDPOINT}/words/${wordId}/guess`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({guess: guess})
+    })
+  }
+
+}
+
+export default AuthApiService
