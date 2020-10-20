46f58db34ca2fedc68acc59ebc89c6a3ad10f189
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-20 12:40:54 -0700

    Added RewardContext provider to index

--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 import { GoalProvider } from './contexts/GoalContext';
+import { RewardProvider } from './contexts/RewardContext';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
-
 
 ReactDOM.render(
     <BrowserRouter>
         <UserProvider>
           <GoalProvider>
-            <App />
+            <RewardProvider>
+              <App />
+            </RewardProvider>
           </GoalProvider>
         </UserProvider>
     </BrowserRouter>,
   document.getElementById('root')
 );
-
 
 serviceWorker.unregister();
