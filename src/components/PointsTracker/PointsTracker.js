ab66b54aad73420edc8ad852708ad84ddd35cf4d
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-21 08:16:14 -0700

    PointTracker component started

---
+++ src/components/PointsTracker/PointsTracker.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import TokenService from '../../services/token-service';
+import UserContext from '../../contexts/UserContext';
+import './PointTracker.css';
+
+export default class PointTracker extends Component {
+  // Point tracker.
+
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
