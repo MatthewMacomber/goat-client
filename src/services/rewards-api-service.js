c581384c3078f485cd16c23d59cfacd04a3b34fb
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-21 07:28:06 -0700

    Rewards api service layout

---
+++ src/services/rewards-api-service.js
@@ -0,0 +1,10 @@
+import config from '../config';
+import TokenService from './token-service';
+
+const RewardsApiService = {
+  getReward() {},
+  addReward() {},
+  modifyReward() {}
+};
+
+export default RewardsApiService;
