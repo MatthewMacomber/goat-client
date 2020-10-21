9ed988bf88c52b5cc799a6c7307399cd2776f102
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-21 07:57:34 -0700

    Complete rewards api service

--- src/services/rewards-api-service.js
+++ src/services/rewards-api-service.js
@@ -2,9 +2,50 @@ import config from '../config';
 import TokenService from './token-service';
 
 const RewardsApiService = {
-  getReward() {},
-  addReward() {},
-  modifyReward() {}
+  getReward() {
+    return fetch(`${config.API_ENDPOINT}/rewards`, {
+      method: 'GET',
+      header: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
+      .then(res => 
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      );
+  },
+  addReward(reward) {
+    return fetch(`${config.API_ENDPOINT}/rewards`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(reward),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e.message))
+          : res.json()
+      );
+  },
+  modifyReward(modify) {
+    return fetch(`${config.API_ENDPOINT}/rewards`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(modify)
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e.message))
+          : res.json()
+      );
+  }
 };
 
 export default RewardsApiService;
