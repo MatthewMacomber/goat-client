5364cb43f925eeafc49b2ac2e2044f887f153f3c
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-22 10:39:24 -0700

    Now a vertical bar with a goat image located at the current points.

--- src/components/GoatAnim/GoatAnim.js
+++ src/components/GoatAnim/GoatAnim.js
@@ -25,8 +25,9 @@ export default class GoatAnim extends Component {
     return (
       <div>
         <div className='goatAnimBar'>
+          <div id='left' className='cliffSection left' style={{'width': maxPercent + '%'}}><p>{max} Points left</p></div>
+          <div className='goatImage'><img src='https://svgsilh.com/svg/1484988.svg' width='50px' alt='Goat head' /></div>
           <div className='cliffSection current' style={{'width': currentPercent + '%'}}></div>
-          <div id='left' className='cliffSection left' style={{'width': maxPercent + '%'}}><p>0{max} Points left</p></div>
         </div>
       </div>
     )
