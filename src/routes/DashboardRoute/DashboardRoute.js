d0d5e66f86690885b5e1fecbfeeae7056cc8ab13
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-27 09:15:10 -0700

    Removed debug file

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import GoalContext from '../../contexts/GoalContext';
 import Accordion from '../../components/Accordion/accordion';
 import RedemptionPopUp from '../../components/RedemptionPopUp/RedemptionPopUp';
