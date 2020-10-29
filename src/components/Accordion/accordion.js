e1df8ebd29c7a53e7f394c4642dc50035e098fba
Justin Foster <jfoster91@gmail.com>
2020-10-28 23:39:38 -0400

    App card

--- src/components/Accordion/accordion.js
+++ src/components/Accordion/accordion.js
@@ -13,14 +13,14 @@ export default class Accordion extends React.Component {
       return html;
     }
     for(let goal of goals){
+      const expired = new Date(goal.end_date).getTime() < new Date().getTime();
       if(goal.id === this.state.selectedId){
-        const expired = new Date(goal.end_date).getTime() < new Date().getTime();
           html.push(
             <div key = {goal.id} className={`goalCard ${expired ? 'expiredGoal' : 'notExpiredGoal'}`}>
               <button className='goalList' onClick={() => {this.setState({selectedId : null})}}>{goal.title}</button>
                 <div>
                   <p>{goal.description}</p>
-                  <p>{goal.points}</p>
+                  <p>Value: {goal.points}</p>
                   <p>{new Date(goal.end_date).toLocaleDateString("en-US")}</p>
                   <p>{goal.complete}</p>
                   {expired && <p className='expireMessage'>This goal has expired. Would you like to mark this as completed or not completed?</p>}
@@ -33,7 +33,7 @@ export default class Accordion extends React.Component {
       } else {
         html.push(
           <div key = {goal.id} className='AccordionFrame'>
-            <button className='AccordionCard' onClick={() => {this.setState({selectedId : goal.id})}}>{goal.title}</button>
+            <button className={`AccordionCard ${expired ? 'expireMessage' : ''}`} onClick={() => {this.setState({selectedId : goal.id})}}>{goal.title}</button>
           </div>
         )
       }
