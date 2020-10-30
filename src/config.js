7af8b87d072c37588af0cb5871fe47ef609fa253
Justin Foster <jfoster91@gmail.com>
2020-10-30 13:58:47 -0400

    Requirement fixes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  //API_ENDPOINT: 'https://polar-springs-61352.herokuapp.com/api',
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://polar-springs-61352.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'goat-client-auth-token'
 }
 //https://polar-springs-61352.herokuapp.com/api
