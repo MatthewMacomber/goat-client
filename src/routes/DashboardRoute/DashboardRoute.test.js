baaa97e36379138277207c7fa614ba8349c32c46
Patrick Easter <epatri6@gmail.com>
2020-10-30 14:08:41 -0400

    Smoke tests

--- src/routes/DashboardRoute/DashboardRoute.test.js
+++ src/routes/DashboardRoute/DashboardRoute.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import LandingPage from './LandingPage';
+import DashboardRoute from './DashboardRoute';
+import {BrowserRouter} from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<BrowserRouter><LandingPage /></BrowserRouter>, div);
+  ReactDOM.render(<BrowserRouter><DashboardRoute /></BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 })
